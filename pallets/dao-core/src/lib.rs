12e8039f424862d575581587c46809ca72a3b592
cute0laf <oliverlim818@gmail.com>
2023-08-03 06:42:00 -0700

    wip: add dao-core

---
+++ pallets/dao-core/src/lib.rs
@@ -0,0 +1,351 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use sp_std::prelude::*;
+
+pub use pallet::*;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+pub mod functions;
+
+mod types;
+pub use types::Dao;
+
+pub use frame_support::{
+	sp_runtime::traits::{One, Saturating},
+	storage::bounded_vec::BoundedVec,
+	traits::{
+		tokens::fungibles::{metadata::Mutate as MetadataMutate, Mutate},
+		Currency,
+	},
+	weights::Weight,
+};
+use pallet_dao_assets::Pallet as Assets;
+
+pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+pub type CurrencyOf<T> = <T as Config>::Currency;
+pub type DepositBalanceOf<T> = <CurrencyOf<T> as Currency<AccountIdOf<T>>>::Balance;
+type AssetIdOf<T> = <T as Config>::AssetId;
+pub type DaoIdOf<T> = BoundedVec<u8, <T as Config>::MaxLengthId>;
+type DaoNameOf<T> = BoundedVec<u8, <T as Config>::MaxLengthName>;
+pub type MetadataOf<T> = BoundedVec<u8, <T as Config>::MaxLengthMetadata>;
+type DaoOf<T> = Dao<DaoIdOf<T>, AccountIdOf<T>, DaoNameOf<T>, AssetIdOf<T>, MetadataOf<T>>;
+
+pub mod weights;
+use weights::WeightInfo;
+
+#[frame_support::pallet]
+pub mod pallet {
+
+	use super::*;
+	use frame_support::{pallet_prelude::*, traits::ReservableCurrency};
+	use frame_system::pallet_prelude::*;
+
+	/// The current storage version.
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
+	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config:
+		frame_system::Config + pallet_contracts::Config + pallet_dao_assets::Config
+	{
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		type Currency: ReservableCurrency<Self::AccountId>;
+		//+ IsType<<Self as pallet_contracts::Config>::Currency>;
+
+		type AssetId: IsType<<Self as pallet_dao_assets::Config>::AssetId>
+			+ Member
+			+ Parameter
+			+ Copy
+			+ Default
+			+ MaxEncodedLen
+			+ One
+			+ Saturating;
+
+		type WeightInfo: WeightInfo;
+
+		#[pallet::constant]
+		type DaoDeposit: Get<DepositBalanceOf<Self>>;
+
+		#[pallet::constant]
+		type MinLength: Get<u32>;
+
+		#[pallet::constant]
+		type MaxLengthId: Get<u32>;
+
+		#[pallet::constant]
+		type MaxLengthName: Get<u32>;
+
+		#[pallet::constant]
+		type MaxLengthMetadata: Get<u32>;
+
+		#[pallet::constant]
+		type TokenUnits: Get<u8>;
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub (super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		DaoCreated {
+			dao_id: DaoIdOf<T>,
+			owner: T::AccountId,
+		},
+		DaoDestroyed {
+			dao_id: DaoIdOf<T>,
+		},
+		DaoTokenIssued {
+			dao_id: DaoIdOf<T>,
+			supply: <T as pallet_dao_assets::Config>::Balance,
+			asset_id: <T as Config>::AssetId,
+		},
+		DaoMetadataSet {
+			dao_id: DaoIdOf<T>,
+		},
+		DaoOwnerChanged {
+			dao_id: DaoIdOf<T>,
+			new_owner: T::AccountId,
+		},
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		DaoIdInvalidLengthTooShort,
+		DaoIdInvalidLengthTooLong,
+		DaoIdInvalidChar,
+		DaoNameInvalidLengthTooShort,
+		DaoNameInvalidLengthTooLong,
+		DaoAlreadyExists,
+		DaoDoesNotExist,
+		DaoSignerNotOwner,
+		DaoTokenAlreadyIssued,
+		MetadataInvalidLengthTooLong,
+		MetadataInvalid,
+		HashInvalidWrongLength,
+	}
+
+	/// Key-Value Store of all _DAOs_, with the key being the `dao_id`.
+	#[pallet::storage]
+	#[pallet::getter(fn get_dao)]
+	pub type Daos<T: Config> = StorageMap<_, Blake2_128Concat, DaoIdOf<T>, DaoOf<T>>;
+
+	/// Internal incrementor of all assets issued by this module.
+	/// The first asset starts with _1_ (sic!, not 0) and then the id is assigned by order of
+	/// creation.
+	#[pallet::storage]
+	#[pallet::getter(fn get_current_asset_id)]
+	pub type CurrentAssetId<T> = StorageValue<_, AssetIdOf<T>, ValueQuery>;
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Create a fresh DAO.
+		///
+		/// - `dao_id`: Unique identifier for the DAO, bounded by _MinLength_ & _MaxLengthId_
+		/// - `dao_name`: Name of the to-be-created DAO, bounded by _MinLength_ & _MaxLengthName_
+		///
+		/// A DAO must reserve the _DaoDeposit_ fee.
+		#[pallet::call_index(1)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::create_dao())]
+		pub fn create_dao(
+			origin: OriginFor<T>,
+			dao_id: Vec<u8>,
+			dao_name: Vec<u8>,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			let id: BoundedVec<_, _> =
+				dao_id.try_into().map_err(|_| Error::<T>::DaoIdInvalidLengthTooLong)?;
+			ensure!(
+				id.len() >= T::MinLength::get() as usize,
+				Error::<T>::DaoIdInvalidLengthTooShort
+			);
+			ensure!(
+				id.iter().all(|b| b.is_ascii_uppercase() || b.is_ascii_digit()),
+				Error::<T>::DaoIdInvalidChar
+			);
+			ensure!(!<Daos<T>>::contains_key(&id), Error::<T>::DaoAlreadyExists);
+
+			let name: BoundedVec<_, _> =
+				dao_name.try_into().map_err(|_| Error::<T>::DaoNameInvalidLengthTooLong)?;
+			ensure!(
+				name.len() >= T::MinLength::get() as usize,
+				Error::<T>::DaoNameInvalidLengthTooShort
+			);
+
+			<T as Config>::Currency::reserve(&sender, <T as Config>::DaoDeposit::get())?;
+
+			Self::deposit_event(Event::DaoCreated { owner: sender.clone(), dao_id: id.clone() });
+			let dao = Dao {
+				id: id.clone(),
+				name,
+				owner: sender,
+				asset_id: None,
+				meta: Default::default(),
+				meta_hash: Default::default(),
+			};
+			<Daos<T>>::insert(id, dao);
+			Ok(())
+		}
+
+		/// Destroy a DAO.
+		///
+		/// - `dao_id`: The DAO to destroy
+		///
+		/// Signer of this TX needs to be the owner of the DAO.
+		#[pallet::call_index(2)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::destroy_dao())]
+		pub fn destroy_dao(origin: OriginFor<T>, dao_id: Vec<u8>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let dao = Self::load_dao(dao_id)?;
+			ensure!(dao.owner == sender, Error::<T>::DaoSignerNotOwner);
+
+			if let Some(asset_id) = dao.asset_id {
+				if let Some(asset) = pallet_dao_assets::Asset::<T>::get(asset_id.into()) {
+					if pallet_dao_assets::AssetStatus::Destroyed != asset.status {
+						Err(Error::<T>::DaoTokenAlreadyIssued)?;
+					}
+				}
+			}
+
+			<T as Config>::Currency::unreserve(&sender, <T as Config>::DaoDeposit::get());
+			Self::deposit_event(Event::DaoDestroyed { dao_id: dao.id.clone() });
+			<Daos<T>>::remove(&dao.id);
+			Ok(())
+		}
+
+		/// Issue the DAO token
+		///
+		/// - `dao_id`: The DAO for which to issue a token
+		/// - `supply`: The total supply of the token to be issued
+		///
+		/// Tokens can only be issued once and the signer of this TX needs to be the owner
+		/// of the DAO.
+		#[pallet::call_index(3)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::issue_token())]
+		pub fn issue_token(
+			origin: OriginFor<T>,
+			dao_id: Vec<u8>,
+			supply: <T as pallet_dao_assets::Config>::Balance,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let dao = Self::load_dao(dao_id)?;
+			ensure!(dao.owner == sender, Error::<T>::DaoSignerNotOwner);
+			ensure!(dao.asset_id.is_none(), Error::<T>::DaoTokenAlreadyIssued);
+
+			// create a fresh asset
+			<CurrentAssetId<T>>::mutate(|asset_id| asset_id.saturating_inc());
+
+			<pallet_dao_assets::pallet::Pallet<T>>::do_force_create(
+				<CurrentAssetId<T>>::get().into(),
+				dao.owner.clone(),
+				One::one(),
+			)?;
+
+			// and distribute it to the owner
+			<pallet_dao_assets::pallet::Pallet<T>>::do_mint(
+				<CurrentAssetId<T>>::get().into(),
+				&dao.owner,
+				supply,
+			)?;
+
+			// set the token metadata to the dao metadata
+			<pallet_dao_assets::pallet::Pallet<T>>::do_set_metadata(
+				<CurrentAssetId<T>>::get().into(),
+				&dao.owner,
+				dao.name.into(),
+				dao.id.clone().into(),
+				<T as Config>::TokenUnits::get(),
+			)?;
+
+			Self::deposit_event(Event::DaoTokenIssued {
+				dao_id: dao.id.clone(),
+				supply,
+				asset_id: <CurrentAssetId<T>>::get(),
+			});
+			// ... and link the dao to the asset
+			<Daos<T>>::try_mutate(dao.id, |maybe_dao| {
+				let d = maybe_dao.as_mut().ok_or(Error::<T>::DaoDoesNotExist)?;
+				d.asset_id = Some(<CurrentAssetId<T>>::get());
+				Ok(())
+			})
+		}
+
+		/// Set metadata
+		///
+		/// - `dao_id`: The DAO for which to set metadata
+		/// - `meta`: HTTP or IPFS address for the metadata about this DAO (description, logo)
+		/// - `hash`: SHA3 hash of the metadata to be found via `meta`
+		#[pallet::call_index(4)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::set_metadata())]
+		pub fn set_metadata(
+			origin: OriginFor<T>,
+			dao_id: Vec<u8>,
+			meta: Vec<u8>,
+			hash: Vec<u8>,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let dao = Self::load_dao(dao_id)?;
+			ensure!(dao.owner == sender, Error::<T>::DaoSignerNotOwner);
+
+			let meta: BoundedVec<_, _> =
+				meta.try_into().map_err(|_| Error::<T>::MetadataInvalidLengthTooLong)?;
+			let hash: BoundedVec<_, _> =
+				hash.try_into().map_err(|_| Error::<T>::HashInvalidWrongLength)?;
+			ensure!(
+				meta.is_empty() && hash.is_empty() || Self::metadata_is_valid(&meta),
+				Error::<T>::MetadataInvalid
+			);
+
+			Self::deposit_event(Event::DaoMetadataSet { dao_id: dao.id.clone() });
+
+			<Daos<T>>::try_mutate(dao.id, |maybe_dao| {
+				let dao = maybe_dao.as_mut().ok_or(Error::<T>::DaoDoesNotExist)?;
+				dao.meta = meta;
+				dao.meta_hash = hash;
+				Ok(())
+			})
+		}
+
+		/// Change owner
+		///
+		/// - `dao_id`: the DAO to transfer ownership of
+		/// - `new_owner`: the new owner
+		#[pallet::call_index(5)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::set_metadata())]
+		pub fn change_owner(
+			origin: OriginFor<T>,
+			dao_id: Vec<u8>,
+			new_owner: T::AccountId,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let dao_id: BoundedVec<_, _> =
+				dao_id.try_into().map_err(|_| Error::<T>::DaoIdInvalidLengthTooLong)?;
+			Daos::<T>::try_mutate(dao_id.clone(), |maybe_dao| -> DispatchResult {
+				let dao = maybe_dao.as_mut().ok_or(Error::<T>::DaoDoesNotExist)?;
+				ensure!(dao.owner == sender, Error::<T>::DaoSignerNotOwner);
+				if dao.owner == new_owner {
+					return Ok(())
+				}
+				// also change asset owner if token was issued
+				if let Some(asset_id) = dao.asset_id {
+					Assets::<T>::change_owner(asset_id.into(), new_owner.clone())?;
+				}
+
+				dao.owner = new_owner.clone();
+				Ok(())
+			})?;
+			Self::deposit_event(Event::DaoOwnerChanged { dao_id, new_owner });
+			Ok(())
+		}
+	}
+}
---
+++ pallets/dao-core/src/tests.rs
@@ -0,0 +1,217 @@
+use crate::{mock::*, Config, Error};
+use frame_support::{assert_noop, assert_ok, traits::TypedGet};
+use pallet_balances::Error as BalancesError;
+
+#[test]
+fn dao_id_valid_chars() {
+	new_test_ext().execute_with(|| {
+		for b in 0u8..255u8 {
+			let mut id = b"GDAO".to_vec();
+			id.push(b);
+			match b {
+				b'A'..=b'Z' | b'0'..=b'9' => {
+					assert_ok!(DaoCore::create_dao(
+						RuntimeOrigin::signed(1),
+						id,
+						b"Genesis DAO".to_vec()
+					));
+				},
+				_ => {
+					assert_noop!(
+						DaoCore::create_dao(RuntimeOrigin::signed(1), id, b"Genesis DAO".to_vec()),
+						Error::<Test>::DaoIdInvalidChar
+					);
+				},
+			}
+		}
+	})
+}
+
+#[test]
+fn it_creates_a_dao() {
+	new_test_ext().execute_with(|| {
+
+		assert_noop!(
+			DaoCore::create_dao(RuntimeOrigin::signed(1), b"XX".to_vec(), b"Genesis DAO".to_vec()),
+			Error::<Test>::DaoIdInvalidLengthTooShort
+		);
+
+		assert_noop!(
+			DaoCore::create_dao(RuntimeOrigin::signed(1), b"XKETGUNIQHQKYYQ7JRQLC7SH02LY7WI27DXOZQURA0M4Z2MRI11L5UZ0DMVRDREQ9TFQI530UB3Z7ZOMMZ10HXAA9TKBMBC1ETFCXHP6HI9G0UXX8SAIYQ0JZI1R1CH9CU7FZHDN50SB3DWMQVD0DAG1BPD52COUUS8JBQSMYOKJDDXU5LMXHELVG5DNZFXKDWSI8XFY605ZLZZAV34OBVUL5770GKF5DS96E0UIPC80IW65E4F7VILBURIIO87CP".to_vec(), b"Genesis DAO".to_vec()),
+			Error::<Test>::DaoIdInvalidLengthTooLong
+		);
+
+		assert_noop!(
+			DaoCore::create_dao(RuntimeOrigin::signed(1), b"GDAO".to_vec(), b"GD".to_vec()),
+			Error::<Test>::DaoNameInvalidLengthTooShort
+		);
+
+		assert_noop!(
+			DaoCore::create_dao(RuntimeOrigin::signed(1),  b"GDAO".to_vec(), b"XKETGUNIQHQKYYQ7JRQLC7SH02LY7WI27DXOZQURA0M4Z2MRI11L5UZ0DMVRDREQ9TFQI530UB3Z7ZOMMZ10HXAA9TKBMBC1ETFCXHP6HI9G0UXX8SAIYQ0JZI1R1CH9CU7FZHDN50SB3DWMQVD0DAG1BPD52COUUS8JBQSMYOKJDDXU5LMXHELVG5DNZFXKDWSI8XFY605ZLZZAV34OBVUL5770GKF5DS96E0UIPC80IW65E4F7VILBURIIO87CP".to_vec()),
+			Error::<Test>::DaoNameInvalidLengthTooLong
+		);
+
+		assert_eq!(Balances::free_balance(1), 1000);
+		assert_ok!(DaoCore::create_dao(RuntimeOrigin::signed(1), b"GDAO".to_vec(), b"Genesis DAO".to_vec()));
+		// reserve taken
+		assert_eq!(Balances::free_balance(1), 990);
+
+		assert_noop!(
+			DaoCore::create_dao(RuntimeOrigin::signed(1), b"GDAO".to_vec(), b"Genesis DAO".to_vec()),
+			Error::<Test>::DaoAlreadyExists
+		);
+
+		// reserve insufficient fails
+		assert_noop!(
+			DaoCore::create_dao(RuntimeOrigin::signed(2), b"GDAO2".to_vec(), b"Genesis DAO 2".to_vec()),
+			BalancesError::<Test>::InsufficientBalance
+		);
+	});
+}
+
+#[test]
+fn it_destroys_a_dao() {
+	new_test_ext().execute_with(|| {
+		assert_noop!(
+			DaoCore::destroy_dao(RuntimeOrigin::signed(1), b"GDAO".to_vec()),
+			Error::<Test>::DaoDoesNotExist
+		);
+
+		assert_ok!(DaoCore::create_dao(
+			RuntimeOrigin::signed(1),
+			b"GDAO".to_vec(),
+			b"Genesis DAO".to_vec()
+		));
+
+		assert_noop!(
+			DaoCore::destroy_dao(RuntimeOrigin::signed(2), b"GDAO".to_vec()),
+			Error::<Test>::DaoSignerNotOwner
+		);
+
+		assert_ok!(DaoCore::issue_token(RuntimeOrigin::signed(1), b"GDAO".to_vec(), 1000));
+
+		assert_noop!(
+			DaoCore::destroy_dao(RuntimeOrigin::signed(1), b"GDAO".to_vec()),
+			Error::<Test>::DaoTokenAlreadyIssued
+		);
+
+		let dao = DaoCore::load_dao(b"GDAO".to_vec()).unwrap();
+		let asset_id = dao.asset_id.unwrap();
+		assert_ok!(Assets::start_destroy(RuntimeOrigin::signed(1), asset_id));
+		assert_ok!(Assets::destroy_accounts(RuntimeOrigin::signed(1), asset_id));
+		assert_ok!(Assets::finish_destroy(RuntimeOrigin::signed(1), asset_id));
+
+		assert_ok!(DaoCore::destroy_dao(RuntimeOrigin::signed(1), b"GDAO".to_vec()));
+
+		assert_noop!(
+			DaoCore::destroy_dao(RuntimeOrigin::signed(1), b"GDAO".to_vec()),
+			Error::<Test>::DaoDoesNotExist
+		);
+	});
+}
+
+#[test]
+fn issues_a_token() {
+	new_test_ext().execute_with(|| {
+		assert_noop!(
+			DaoCore::issue_token(RuntimeOrigin::signed(1), b"NONE".to_vec(), 1000),
+			Error::<Test>::DaoDoesNotExist
+		);
+
+		assert_ok!(DaoCore::create_dao(
+			RuntimeOrigin::signed(1),
+			b"GDAO".to_vec(),
+			b"Genesis DAO".to_vec()
+		));
+		assert_ok!(DaoCore::issue_token(RuntimeOrigin::signed(1), b"GDAO".to_vec(), 1000));
+
+		let dao = DaoCore::load_dao(b"GDAO".to_vec()).unwrap();
+		let asset_id = dao.asset_id.unwrap();
+
+		assert_eq!(asset_id, 1);
+
+		use frame_support::traits::tokens::fungibles::metadata::Inspect;
+		assert_eq!(Assets::name(asset_id), b"Genesis DAO".to_vec());
+		assert_eq!(Assets::symbol(asset_id), b"GDAO".to_vec());
+		assert_eq!(Assets::decimals(asset_id), <Test as Config>::TokenUnits::get());
+
+		// payout
+		assert_eq!(Assets::balance(asset_id, &dao.owner), 1000);
+
+		// increment
+		assert_ok!(DaoCore::create_dao(
+			RuntimeOrigin::signed(1),
+			b"GDAO2".to_vec(),
+			b"Genesis DAO 2".to_vec()
+		));
+		assert_ok!(DaoCore::issue_token(RuntimeOrigin::signed(1), b"GDAO2".to_vec(), 1000));
+
+		let dao2 = DaoCore::load_dao(b"GDAO2".to_vec()).unwrap();
+		let asset_id2 = dao2.asset_id.unwrap();
+		assert_eq!(asset_id2, 2);
+	});
+}
+
+#[test]
+fn it_sets_metadata() {
+	new_test_ext().execute_with(|| {
+		let metadata = b"http://my.cool.dao".to_vec();
+		// https://en.wikipedia.org/wiki/SHA-3#Examples_of_SHA-3_variants
+		let hash = b"a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a".to_vec();
+
+		assert_noop!(
+			DaoCore::set_metadata(
+				RuntimeOrigin::signed(1),
+				b"NONE".to_vec(),
+				metadata.clone(),
+				hash.clone()
+			),
+			Error::<Test>::DaoDoesNotExist
+		);
+
+		assert_ok!(DaoCore::create_dao(
+			RuntimeOrigin::signed(1),
+			b"GDAO".to_vec(),
+			b"Genesis DAO".to_vec()
+		));
+		assert_ok!(DaoCore::set_metadata(
+			RuntimeOrigin::signed(1),
+			b"GDAO".to_vec(),
+			metadata.clone(),
+			hash.clone()
+		));
+
+		let dao = DaoCore::load_dao(b"GDAO".to_vec()).unwrap();
+
+		assert_eq!(dao.meta, metadata);
+		assert_eq!(dao.meta_hash, hash);
+	});
+}
+
+#[test]
+fn can_change_owner() {
+	new_test_ext().execute_with(|| {
+		let new_owner = 61;
+		assert_noop!(
+			DaoCore::change_owner(RuntimeOrigin::signed(1), b"NONE".to_vec(), new_owner),
+			Error::<Test>::DaoDoesNotExist
+		);
+
+		// create DAO
+		let creator = 1; // this account has sufficient balance to create a DAO
+		assert_ok!(DaoCore::create_dao(
+			RuntimeOrigin::signed(creator),
+			b"GDAO".to_vec(),
+			b"Genesis DAO".to_vec()
+		));
+
+		let dao = DaoCore::load_dao(b"GDAO".to_vec()).unwrap();
+		assert_eq!(dao.owner, creator);
+
+		// change owner
+		assert_ok!(DaoCore::change_owner(RuntimeOrigin::signed(1), b"GDAO".to_vec(), new_owner));
+
+		let dao = DaoCore::load_dao(b"GDAO".to_vec()).unwrap();
+		assert_eq!(dao.owner, new_owner);
+	});
+}
